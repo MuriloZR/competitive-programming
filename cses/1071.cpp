0d79b71b91c13213e36ee027f2fe018195b47814
MuriloZR <murilodojogo@gmail.com>
2026-03-21 15:50:29 -0300

    more submissions

--- cses/1071.cpp
+++ cses/1071.cpp
@@ -3,10 +3,11 @@
 #define rv(v) for(auto& i : v) cin >> i
 #define EPS 1e-9
 #define MOD 1e9 + 7
+#define MODW 998244353
 
 using namespace std;
+using ll = long long;
 
-//not ready yet
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -14,9 +15,14 @@ int main() {
     int t; cin >> t;
     while (t--)
     {
-        int y, x; cin >> y >> x;
-        unsigned long long n = 1+(max(x,y)-1)*(max(x,y));
-        if ((x-1)%2==0)
-        cout << n << endl;
+        ll y, x; cin >> y >> x;
+        if (y > x) {
+            if (y%2 == 0) cout << y*y -x + 1 << endl;
+            else cout << (y-1)*(y-1) + x << endl;
+        }
+        else {
+            if (x%2 == 0) cout << (x-1)*(x-1) + y << endl;
+            else cout << x*x - y + 1 << endl;
+        }
     }
 }
