63243e14fe0515a89e29a2979dd55efe57f7a96e
MuriloZR <murilodojogo@gmail.com>
2026-03-24 14:16:11 -0300

    more submissions

---
+++ beecrowd/1039.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define rv(v) for(auto& i : v) cin >> i
+#define EPS 1e-9
+#define MOD 1e9 + 7
+#define MODW 998244353
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll r1, x1, y1, r2, x2, y2;
+    while (cin >> r1 >> x1 >> y1 >> r2 >> x2 >> y2) {
+        double d = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+        cout << (r1 >= d + r2 ? "RICO\n" : "MORTO\n");
+    }
+}
